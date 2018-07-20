12d86ed94039a48c5abad09c60cec7a147213012
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-20 11:41:17 +0100

    add a test

---
+++ tests/test_qtwirl_with_sample.py
@@ -0,0 +1,52 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import os
+import numpy as np
+import pandas as pd
+
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+from pandas.util.testing import assert_frame_equal
+
+import alphatwirl
+
+from qtwirl import qtwirl
+
+##__________________________________________________________________||
+def test_with_sample():
+
+    ##
+    sample_dir = os.path.join(os.path.dirname(__file__), 'data')
+    sample_basenames = [
+        'sample_chain_01.root',
+        'sample_chain_02.root',
+        # 'sample_chain_03.root',
+        'sample_chain_04.root',
+    ]
+    sample_paths = [os.path.join(sample_dir, b) for b in sample_basenames]
+
+    ##
+    tbl_dir = os.path.join(os.path.dirname(__file__), 'tbl')
+    tbl_path = os.path.join(tbl_dir, 'tbl_n.jet_pt-w.txt')
+    tbl = pd.read_table(tbl_path, delim_whitespace=True)
+
+    ##
+    RoundLog = alphatwirl.binning.RoundLog
+    reader_cfg = dict(
+        summarizer=dict(
+            keyAttrNames=('jet_pt', ),
+            binnings=(RoundLog(0.1, 100), ),
+            keyIndices=('*', ),
+            keyOutColumnNames=('jet_pt', )
+        ))
+
+    results = qtwirl(file=sample_paths, reader_cfg=reader_cfg, tree_name='tree')
+
+    ##
+    assert_frame_equal(tbl, results[0],check_names=True)
+
+##__________________________________________________________________||
