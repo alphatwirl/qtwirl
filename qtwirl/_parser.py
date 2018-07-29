3c32f2974fb1945968e1e034bd1cf5bdb21e8c18
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-29 16:12:08 +0200

    implement parse_reader_cfg() for table_cfg

--- qtwirl/_parser.py
+++ qtwirl/_parser.py
@@ -10,6 +10,33 @@ def parse_file(file):
 
 ##__________________________________________________________________||
 def parse_reader_cfg(reader_cfg):
-    return reader_cfg
+
+    if _is_dict(reader_cfg):
+        return [_wrap_table_cfg(reader_cfg)]
+
+    ret = [_wrap_table_cfg(c) for c in reader_cfg]
+
+    return ret
+
+def _is_dict(obj):
+    try:
+        # check for mixin methods of mapping
+        # https://docs.python.org/3.6/library/collections.abc.html
+        obj.keys()
+        obj.items()
+        obj.values()
+    except AttributeError:
+        return False
+
+    return True
+
+def _wrap_table_cfg(cfg):
+    config_keys = ('table_cfg',)
+    default_config_key = 'table_cfg'
+    if len(cfg) == 1 and list(cfg.keys())[0] in config_keys:
+        # already wrapped
+        return cfg
+
+    return {default_config_key: cfg}
 
 ##__________________________________________________________________||
---
+++ tests/test_parser_reader_cfg.py
@@ -0,0 +1,68 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+import alphatwirl
+
+from qtwirl._parser import parse_reader_cfg
+
+##__________________________________________________________________||
+RoundLog = mock.Mock()
+
+tblcfg_dict1 = dict(
+    keyAttrNames=('jet_pt', ),
+    binnings=(RoundLog(0.1, 100), ),
+    keyIndices=('*', ),
+)
+
+tblcfg_dict2 = dict(
+    keyAttrNames=('met', ),
+    binnings=(RoundLog(0.1, 100), ),
+)
+
+##__________________________________________________________________||
+@pytest.mark.parametrize('arg, expected', [
+    pytest.param(
+        dict(), [dict(table_cfg=dict())], id='empty-dict'
+    ),
+    pytest.param(
+        [ ], [ ], id='empty-list'
+    ),
+    pytest.param(
+        dict(tblcfg_dict1),
+        [dict(table_cfg=dict(tblcfg_dict1))],
+        id='dict-short'
+    ),
+    pytest.param(
+        dict(table_cfg=dict(tblcfg_dict1)),
+        [dict(table_cfg=dict(tblcfg_dict1))],
+        id='dict-full'
+    ),
+    pytest.param(
+        [dict(tblcfg_dict1)],
+        [dict(table_cfg=dict(tblcfg_dict1))],
+        id='list-with-one-dict'
+    ),
+    pytest.param(
+        [
+            dict(table_cfg=dict(tblcfg_dict1)),
+            dict(table_cfg=dict(tblcfg_dict2)),
+        ],
+        [
+            dict(table_cfg=dict(tblcfg_dict1)),
+            dict(table_cfg=dict(tblcfg_dict2)),
+        ],
+        id='two_table_cfgs'
+    ),
+])
+def test_parse_reader_cfg(arg, expected):
+    # reader
+    # selection_cfg
+    # table_cfg
+    assert expected == parse_reader_cfg(arg)
+
+##__________________________________________________________________||
