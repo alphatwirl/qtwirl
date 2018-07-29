194e4f74de0fee98746b9717b22a07bf3704d6f9
Tai Sakuma <tai.sakuma@gmail.com>
2018-07-29 20:45:47 +0200

    add a scribbler to test

--- tests/test_qtwirl_with_sample.py
+++ tests/test_qtwirl_with_sample.py
@@ -36,7 +36,7 @@ def test_with_sample_one_dict():
     tbl_dir = os.path.join(os.path.dirname(__file__), 'tbl')
     tbl_paths = [
         os.path.join(tbl_dir, '00', 'tbl_n.jet_pt-w.txt'),
-        os.path.join(tbl_dir, '00', 'tbl_n.met.txt')
+        os.path.join(tbl_dir, '00', 'tbl_n.met.txt'),
     ]
     tbls = [pd.read_table(p, delim_whitespace=True) for p in tbl_paths]
 
@@ -77,12 +77,14 @@ def test_with_sample():
     tbl_dir = os.path.join(os.path.dirname(__file__), 'tbl')
     tbl_paths = [
         os.path.join(tbl_dir, '01', 'tbl_n.jet_pt-w.txt'),
-        os.path.join(tbl_dir, '01', 'tbl_n.met.txt')
+        os.path.join(tbl_dir, '01', 'tbl_n.met.txt'),
+        os.path.join(tbl_dir, '01', 'tbl_n.ht.txt'),
     ]
     tbls = [pd.read_table(p, delim_whitespace=True) for p in tbl_paths]
 
     ##
     RoundLog = alphatwirl.binning.RoundLog
+    from scribblers.essentials import FuncOnNumpyArrays
     reader_cfg = [
         dict(selection_cfg=dict(All=('ev: ev.njets[0] > 4', ))),
         dict(keyAttrNames=('jet_pt', ),
@@ -90,6 +92,12 @@ def test_with_sample():
              keyIndices=('*', ),
              keyOutColumnNames=('jet_pt', )),
         dict(keyAttrNames=('met', ),
+             binnings=(RoundLog(0.1, 100), )),
+        dict(reader=FuncOnNumpyArrays(
+            src_arrays=['jet_pt'],
+            out_name='ht',
+            func=np.sum)),
+        dict(keyAttrNames=('ht', ),
              binnings=(RoundLog(0.1, 100), )),
     ]
 
@@ -105,5 +113,6 @@ def test_with_sample():
     ##
     assert_frame_equal(tbls[0], results[0], check_names=True)
     assert_frame_equal(tbls[1], results[1], check_names=True, check_less_precise=True)
+    assert_frame_equal(tbls[2], results[2], check_names=True)
 
 ##__________________________________________________________________||
