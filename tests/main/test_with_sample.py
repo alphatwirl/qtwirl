3bc4b263f5a8c07e8c35de3b9b71c5661c4c3475
Tai Sakuma <tai.sakuma@gmail.com>
2018-08-12 10:21:00 +0100

    clean up code in test_with_sample.py

--- tests/main/test_with_sample.py
+++ tests/main/test_with_sample.py
@@ -5,11 +5,6 @@ import pandas as pd
 
 import pytest
 
-try:
-    import unittest.mock as mock
-except ImportError:
-    import mock
-
 from pandas.util.testing import assert_frame_equal
 
 import alphatwirl
@@ -20,33 +15,31 @@ from qtwirl import qtwirl
 pytestmark = pytest.mark.filterwarnings('ignore::RuntimeWarning')
 
 ##__________________________________________________________________||
+TESTDATADIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
+
+SAMPLE_ROOT_FILE_DIR = os.path.join(TESTDATADIR, 'root')
+SAMPLE_ROOT_FILE_NAMES = [
+    'sample_chain_01.root',
+    'sample_chain_02.root',
+    'sample_chain_03_zombie.root',
+    'sample_chain_04.root',
+]
+SAMPLE_ROOT_FILE_PATHS = [os.path.join(SAMPLE_ROOT_FILE_DIR, n) for n in SAMPLE_ROOT_FILE_NAMES]
+
+##__________________________________________________________________||
 def test_with_sample_one_dict():
 
     ##
-    sample_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'root')
-    sample_basenames = [
-        'sample_chain_01.root',
-        'sample_chain_02.root',
-        'sample_chain_03_zombie.root',
-        'sample_chain_04.root',
-    ]
-    sample_paths = [os.path.join(sample_dir, b) for b in sample_basenames]
-
-    ##
-    tbl_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'tbl')
-    tbl_paths = [
-        os.path.join(tbl_dir, '00', 'tbl_n.jet_pt-w.txt'),
-        os.path.join(tbl_dir, '00', 'tbl_n.met.txt'),
-    ]
-    tbls = [pd.read_table(p, delim_whitespace=True) for p in tbl_paths]
+    sample_paths = SAMPLE_ROOT_FILE_PATHS
 
     ##
     RoundLog = alphatwirl.binning.RoundLog
     reader_cfg = dict(
-        key_name=('jet_pt', ),
-        key_binning=(RoundLog(0.1, 100), ),
-        key_index=('*', ),
-        key_out_name=('jet_pt', ))
+        key_name='jet_pt',
+        key_binning=RoundLog(0.1, 100),
+        key_index='*',
+        key_out_name='jet_pt'
+    )
 
     results = qtwirl(
         file=sample_paths,
@@ -58,42 +51,30 @@ def test_with_sample_one_dict():
     )
 
     ##
-    assert_frame_equal(tbls[0], results, check_names=True)
+    tbl_dir = os.path.join(TESTDATADIR, 'tbl')
+    tbl_path = os.path.join(tbl_dir, '00', 'tbl_n.jet_pt-w.txt')
+    tbl = pd.read_table(tbl_path, delim_whitespace=True)
+
+    ##
+    assert_frame_equal(tbl, results, check_names=True)
 
 ##__________________________________________________________________||
 def test_with_sample():
 
     ##
-    sample_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'root')
-    sample_basenames = [
-        'sample_chain_01.root',
-        'sample_chain_02.root',
-        'sample_chain_03_zombie.root',
-        'sample_chain_04.root',
-    ]
-    sample_paths = [os.path.join(sample_dir, b) for b in sample_basenames]
-
-    ##
-    tbl_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'tbl')
-    tbl_paths = [
-        os.path.join(tbl_dir, '01', 'tbl_n.jet_pt-w.txt'),
-        os.path.join(tbl_dir, '01', 'tbl_n.met.txt'),
-        os.path.join(tbl_dir, '01', 'tbl_n.njets.met.txt'),
-        os.path.join(tbl_dir, '01', 'tbl_n.ht.txt'),
-    ]
-    tbls = [pd.read_table(p, delim_whitespace=True) for p in tbl_paths]
+    sample_paths = SAMPLE_ROOT_FILE_PATHS
 
     ##
     RoundLog = alphatwirl.binning.RoundLog
     from scribblers.essentials import FuncOnNumpyArrays
     reader_cfg = [
         dict(selection_cfg=dict(All=('ev: ev.njets[0] > 4', ))),
-        dict(key_name=('jet_pt', ),
-             key_binning=(RoundLog(0.1, 100), ),
-             key_index=('*', ),
-             key_out_name=('jet_pt', )),
-        dict(key_name=('met', ),
-             key_binning=(RoundLog(0.1, 100), )),
+        dict(key_name='jet_pt',
+             key_binning=RoundLog(0.1, 100),
+             key_index='*',
+             key_out_name='jet_pt'),
+        dict(key_name='met',
+             key_binning=RoundLog(0.1, 100)),
         dict(
             key_name=('njets', 'met'),
             key_binning=(None, RoundLog(0.2, 100, min=50, underflow_bin=0))), # use None
@@ -101,8 +82,8 @@ def test_with_sample():
             src_arrays=['jet_pt'],
             out_name='ht',
             func=np.sum)),
-        dict(key_name=('ht', ),
-             key_binning=(RoundLog(0.1, 100), )),
+        dict(key_name='ht',
+             key_binning=RoundLog(0.1, 100)),
     ]
 
     results = qtwirl(
@@ -116,6 +97,16 @@ def test_with_sample():
 
     #
     assert 4 == len(results)
+
+    ##
+    tbl_dir = os.path.join(TESTDATADIR, 'tbl')
+    tbl_paths = [
+        os.path.join(tbl_dir, '01', 'tbl_n.jet_pt-w.txt'),
+        os.path.join(tbl_dir, '01', 'tbl_n.met.txt'),
+        os.path.join(tbl_dir, '01', 'tbl_n.njets.met.txt'),
+        os.path.join(tbl_dir, '01', 'tbl_n.ht.txt'),
+    ]
+    tbls = [pd.read_table(p, delim_whitespace=True) for p in tbl_paths]
 
     ##
     assert_frame_equal(tbls[0], results[0], check_names=True)
