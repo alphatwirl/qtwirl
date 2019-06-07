580ee9cd14b78f6d82ca3db595cc55d0c7a04fe0
Tai Sakuma <tai.sakuma@gmail.com>
2019-06-07 10:30:59 +0100

    clean code in test_config_expander_default.py

--- tests/expander/test_config_expander_default.py
+++ tests/expander/test_config_expander_default.py
@@ -10,45 +10,61 @@ def expand_abc_cfg(cfg, shared):
     return {key: dict(expanded=cfg)}
 
 ##__________________________________________________________________||
-def test_config_expander():
+@pytest.fixture()
+def expand_config():
     expand_func_map = {
         'abc_cfg': expand_abc_cfg,
     }
     config_keys = ['def_cfg']
     default_config_key = 'abc_cfg'
-    expand_config = config_expander(
+
+    ret = config_expander(
         expand_func_map=expand_func_map,
         config_keys=config_keys,
         default_config_key=default_config_key
     )
+    return ret
 
-    cfg = [
-        dict(set_default=dict(
-            abc_cfg=dict(A=5, C=4),
-            def_cfg=dict(D=2, E=9),
-        )),
-        dict(A=1, B=2)
-    ]
+##__________________________________________________________________||
+params = [
+    (
+        [
+            dict(
+                set_default=dict(
+                    abc_cfg=dict(A=5, C=4),
+                    def_cfg=dict(D=2, E=9),
+                )),
+            dict(A=1, B=2)
+        ],
+        [
+            dict(abc_cfg=dict(expanded=dict(A=1, B=2, C=4)))
+        ]
+    ),
 
-    expected = [{'abc_cfg': {'expanded': dict(A=1, B=2, C=4)}}]
-    assert expected == expand_config(cfg)
+    (
+        [
+            dict(set_default=dict(A=5, C=4)),
+            dict(A=1, B=2)
+        ],
+        [
+            dict(abc_cfg=dict(expanded=dict(A=1, B=2, C=4)))
+        ]
+    ),
 
-    cfg = [
-        dict(set_default=dict(A=5, C=4)),
-        dict(A=1, B=2)
-    ]
+    (
+        [
+            dict(set_default=dict(abc_cfg=dict(B=3, C=2, E=8))),
+            dict(set_default=dict(A=5, C=4)),
+            dict(A=1, B=2),
+        ],
+        [
+            dict(abc_cfg=dict(expanded=dict(A=1, B=2, C=4, E=8)))
+        ]
+        )
+]
 
-    expected = [{'abc_cfg': {'expanded': dict(A=1, B=2, C=4)}}]
-    assert expected == expand_config(cfg)
-
-    cfg = [
-        dict(set_default=dict(abc_cfg=dict(B=3, C=2, E=8))),
-        dict(set_default=dict(A=5, C=4)),
-        dict(A=1, B=2),
-    ]
-    expected = [
-        {'abc_cfg': {'expanded': dict(A=1, B=2, C=4, E=8)}},
-    ]
+@pytest.mark.parametrize('cfg, expected', params)
+def test_config_expander(cfg, expected, expand_config):
     assert expected == expand_config(cfg)
 
 ##__________________________________________________________________||
